4a8dc60e19e31b11269e8bfc79b33e2e84a5b721
Pratyush Kumar <36464949+pratyushkumar211@users.noreply.github.com>
2021-01-16 18:09:22 -0800

    ok, that classes/functions are working so far.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
 
+    string name;
     cout << "Hello World!" << endl;
-
+    cout << "Please enter your name: \n"; 
+    getline(cin, name); 
+    cout << "Hello, " << name << "\n"; 
+  
+    return 0; 
 }
